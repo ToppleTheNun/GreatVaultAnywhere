51c66ffdca09949459179ee33beb161605a7738f
Richard Harrah <topplethenunnery@gmail.com>
2020-12-24 15:15:57 -0500

    switch back to lua from TS

--- PortableGreatVault.lua
+++ PortableGreatVault.lua
@@ -1,4 +1,4 @@
-GreatVaultAnywhere = LibStub("AceAddon-3.0"):NewAddon("GreatVaultAnywhere", "AceConsole-3.0")
+PortableGreatVault = LibStub("AceAddon-3.0"):NewAddon("PortableGreatVault", "AceConsole-3.0")
 
 local defaults = {
     profile = {
@@ -8,27 +8,27 @@ local defaults = {
     }
 }
 
-function GreatVaultAnywhere:ConstructMinimapIcon()
+function PortableGreatVault:ConstructMinimapIcon()
     self.minimap = {}
-    self.minimap.icon_data = LibStub("LibDataBroker-1.1"):NewDataObject("GreatVaultAnywhere", {
+    self.minimap.icon_data = LibStub("LibDataBroker-1.1"):NewDataObject("PortableGreatVault", {
         type = "data source",
-        text = "GreatVaultAnywhere",
+        text = "PortableGreatVault",
         icon = "Interface\\Icons\\INV_Misc_Note_03",
         OnClick = function(self, button)
             LoadAddOn("Blizzard_WeeklyRewards")
             WeeklyRewardsFrame:Show()
         end,
         OnTooltipShow = function(tooltip)
-            tooltip:SetText("GreatVaultAnywhere")
+            tooltip:SetText("PortableGreatVault")
             tooltip:AddLine("Open the Great Vault", 1, 1, 1)
             tooltip:Show()
         end
     })
     self.minimap.icon = LibStub("LibDBIcon-1.0")
-    self.minimap.icon:Register("GreatVaultAnywhere", self.minimap.icon_data, self.db.profile.minimap)
+    self.minimap.icon:Register("PortableGreatVault", self.minimap.icon_data, self.db.profile.minimap)
 end
 
-function GreatVaultAnywhere:OnInitialize()    
-    self.db = LibStub("AceDB-3.0"):New("GreatVaultAnywhereDB", defaults, true)
+function PortableGreatVault:OnInitialize()    
+    self.db = LibStub("AceDB-3.0"):New("PortableGreatVaultDB", defaults, true)
     self:ConstructMinimapIcon()
 end
